d12ab4788d9a75c16664b8055550345b5e220ba2
Hendrik Barnard <hendrikbarnard@capitecbank.co.za>
2020-09-29 21:11:58 +0200

    Organizing the interface

---
+++ platform/marshalling.go
@@ -0,0 +1,48 @@
+package platform
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+var (
+	JsonMarshaller JsonMarshallerOrganizer
+)
+
+// This is just to make the platform interface nice
+type JsonMarshallerOrganizer struct {
+}
+
+func (j *JsonMarshallerOrganizer) ReadJsonRequest(requestBody io.ReadCloser, outputType interface{}) error {
+	defer requestBody.Close()
+
+	data, err := ioutil.ReadAll(requestBody)
+	if err != nil {
+		Logger.Error("Error reading request body", zap.Error(err))
+		return err
+	}
+
+	err = json.Unmarshal(data, &outputType)
+	if err != nil {
+		Logger.Error("Error unmarshalling response", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
+
+func (j *JsonMarshallerOrganizer) WriteJsonResponse(w http.ResponseWriter, statuscode int, response interface{}) {
+	responseData, err := json.Marshal(response)
+	if err != nil {
+		Logger.Error("Unable to marshal response object", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(statuscode)
+	w.Write(responseData)
+}
